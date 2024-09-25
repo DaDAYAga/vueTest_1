f1b274ed9c5b0fbd5ddcd1d36c9b8f59851f190e
Dylan <f2e-dylan@ibest.com.tw>
2024-09-25 14:11:30 +0800

    240925_init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <div class="wrapper"></div>
+    </header>
+
+    <!-- <nav>
+			<RouterLink to="/">Home</RouterLink>
+			<RouterLink to="/about">About</RouterLink>
+		</nav> -->
+    <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
