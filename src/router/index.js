ea4d0ca8b74c8b1702fbdf4937711d4321970919
DaDAYAga <ppoo33229@gmail.com>
2025-01-09 22:02:35 +0800

    router手動格式化

--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,27 @@ import AndroidView from '../views/AndroidView.vue'
 import IosView from '../views/IosView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/android',
-        name: 'android',
-        // route level code-splitting
-        // this generates a separate chunk (About.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: AndroidView
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
         },
-    {
-        path: '/ios',
-        name: 'ios',
-        component: IosView
-    }
-  ]
+        {
+            path: '/android',
+            name: 'android',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: AndroidView
+        },
+        {
+            path: '/ios',
+            name: 'ios',
+            component: IosView
+        }
+    ]
 })
 
 export default router
